a600d841576eb1caa60da5405626db95a94de1bd
casqan <marcmatija.mm@gmail.com>
2024-10-27 14:43:32 +0100

    added springboot module to files

---
+++ fantasychess-server/src/test/java/com/bteam/fantasychess_server/FantasychessServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bteam.fantasychess_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FantasychessServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
