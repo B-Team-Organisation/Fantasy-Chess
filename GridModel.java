92bfd6b3524fc9e831575a8b34578e30c5bf1bbf
Vukelaj Albano <albigoal@outlook.com>
2024-10-29 22:13:38 +0100

    example GridModel.java

---
+++ GridModel.java
@@ -0,0 +1,28 @@
+/**
+ * Feature: Grid Model
+ *    First Choice ->2d array 
+ *         ->(no other choice, more complex graph represantion node based).
+ *
+ * MxN Grid with m=n
+ *    First Choice(9x9) -> odd number ?
+ *        Second Choice -> 8x8 with a max of 10 pieces per player? 10x10 for 12.
+ *
+ * Implementation Details:
+ * - The grid model  provides
+ *   methods to set, remove, and check the occupancy of cells.
+ * - The Tile class represents an individual cell on the game board and stores
+ *   information about the current piece , maybe about the stats too.
+ * 
+ * Possible Methods requiredd:
+ *     -Set Piece(row, column,->(obj) piece( charachter object with name ,stats))
+ *     -remove Piece( maybe just move) same....
+ *     -boolean occupied (cell obejct (col , row)) is charachter or not
+ *      
+ *
+
+
+ @version 1.0
+ * @since 2024-10-29
+ */
+public class GridModel{
+}
