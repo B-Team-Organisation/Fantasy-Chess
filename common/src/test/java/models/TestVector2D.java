54de44d27e91102af160bd710a37dd015d026d0f
jschw001 <jacinto.schwarzwaelder@student.hs-rm.de>
2024-10-30 11:23:01 +0100

    [Test] adding tests for Vector2D

---
+++ common/src/test/java/models/TestVector2D.java
@@ -0,0 +1,32 @@
+package models;
+
+import org.junit.jupiter.api.Test;
+import main.java.models.Vector2D;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class TestVector2D {
+    Vector2D vec1 = new Vector2D(1, 2);
+    Vector2D vec2 = new Vector2D(3, 4);
+
+    @Test
+    void testAddition() {
+        Vector2D expected = new Vector2D(4, 6);
+        Vector2D actual = vec1.add(vec2);
+        assertEquals(
+                expected,
+                actual,
+                "Vector should be " + expected + ", but was " + actual
+        );
+    }
+
+    @Test
+    void testSubtraction() {
+        Vector2D expected = new Vector2D(2, 2);
+        Vector2D actual = vec2.subtract(vec1);
+        assertEquals(
+                expected,
+                actual,
+               "Vector should be " + expected + ", but was " + actual
+        );
+    }
+}
