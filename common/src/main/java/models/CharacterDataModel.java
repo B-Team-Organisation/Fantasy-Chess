b3d264aeb11ada2005404026e5862baec69632f8
Jacinto Schwarzwälder <schwarzwaelder.jacinto@gmail.com>
2024-11-01 20:21:52 +0100

    [Feature] Adding CharacterDataModel

--- common/src/main/java/models/CharacterDataModel.java
+++ common/src/main/java/models/CharacterDataModel.java
@@ -2,21 +2,27 @@ package models;
 
 import entities.CharacterEntity;
 
+import java.util.Objects;
+
 /**
- * Represents character data stored as a model.
+ * Data model for character entities
+ * <p>
+ * This class represents a Data Transferable Object for static character data.
+ * It contains static attributes about a character.
+ *
+ * @author Jacinto
+ * @version 1.0
  */
 public class CharacterDataModel {
     private CharacterEntity character;
     private String name;
     private int health;
     private int damage;
-    // private AttackMovementPatter;
-    // private MovementPattern;
 
     /**
      * Creates a new CharacterDataModel.
      *
-     * @param character - reference to the character entity
+     * @param character - reference to the {@link CharacterEntity}
      * @param name - name of the character
      * @param health - the health of the character
      * @param damage - the damage from the character
@@ -26,6 +32,62 @@ public class CharacterDataModel {
         this.name = name;
         this.health = health;
         this.damage = damage;
+    }
+
+    public CharacterEntity getCharacter() {
+        return this.character;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getHealth() {
+        return this.health;
+    }
+
+    public int getDamage() {
+        return this.damage;
+    }
+
+    public void setCharacter(CharacterEntity character) {
+        this.character = character;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+    @Override
+    public String toString() {
+        return "CharacterDataModel "
+                + "[character=" + character.toString()
+                + ", name=" + name
+                + ", health=" + health
+                + ", damage=" + damage
+                + "]";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return o instanceof CharacterDataModel
+                && ((CharacterDataModel) o).character.equals(this.character)
+                && ((CharacterDataModel) o).name.equals(this.name)
+                && ((CharacterDataModel) o).health == this.health
+                && ((CharacterDataModel) o).damage == this.damage;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(character, name, health, damage);
     }
 
 }
