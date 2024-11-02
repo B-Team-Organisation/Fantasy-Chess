aa9c07f412d861894fd5dcb7ffe7da4a88c1b545
Jacinto Schwarzwälder <schwarzwaelder.jacinto@gmail.com>
2024-11-02 08:04:47 +0100

    [Feature] Adding AttackData and MovementData.

--- common/src/main/java/models/CharacterDataModel.java
+++ common/src/main/java/models/CharacterDataModel.java
@@ -9,6 +9,9 @@ import java.util.Objects;
  * <p>
  * This class represents a Data Transferable Object for static character data.
  * It contains static attributes about a character.
+ * <p>
+ * A {@link CharacterDataModel} can only have one of {@link AttackDataModel} and {@link MovementDataModel}.
+ * This is done to adhere to the game logic.
  *
  * @author Jacinto
  * @version 1.0
@@ -18,9 +21,46 @@ public class CharacterDataModel {
     private String name;
     private int health;
     private int damage;
+    private AttackDataModel attackData;
+    private MovementDataModel movementData;
+
 
     /**
-     * Creates a new CharacterDataModel.
+     * Creates a new CharacterDataModel with attack data.
+     *
+     * @param character - reference to the {@link CharacterEntity}
+     * @param name - name of the character
+     * @param health - the health of the character
+     * @param damage - the damage from the character
+     * @param attackData - the attack pattern that the character is set to
+     */
+    public CharacterDataModel(CharacterEntity character, String name, int health, int damage, AttackDataModel attackData) {
+        this.character = character;
+        this.name = name;
+        this.health = health;
+        this.damage = damage;
+        this.attackData = attackData;
+    }
+
+    /**
+     * Creates a new CharacterDataModel with movement data.
+     *
+     * @param character - reference to the {@link CharacterEntity}
+     * @param name - name of the character
+     * @param health - the health of the character
+     * @param damage - the damage from the character
+     * @param movementData - the movement pattern that the character is set to
+     */
+    public CharacterDataModel(CharacterEntity character, String name, int health, int damage, MovementDataModel movementData) {
+        this.character = character;
+        this.name = name;
+        this.health = health;
+        this.damage = damage;
+        this.movementData = movementData;
+    }
+
+    /**
+     * Creates a new CharacterDataModel without movement or attack data.
      *
      * @param character - reference to the {@link CharacterEntity}
      * @param name - name of the character
@@ -50,6 +90,14 @@ public class CharacterDataModel {
         return this.damage;
     }
 
+    public AttackDataModel getAttackData() {
+        return this.attackData;
+    }
+
+    public MovementDataModel getMovementData() {
+        return this.movementData;
+    }
+
     public void setCharacter(CharacterEntity character) {
         this.character = character;
     }
@@ -66,6 +114,14 @@ public class CharacterDataModel {
         this.damage = damage;
     }
 
+    public void setAttackData(AttackDataModel attackData) {
+        this.attackData = attackData;
+    }
+
+    public void setMovementData(MovementDataModel movementData) {
+        this.movementData = movementData;
+    }
+
     @Override
     public String toString() {
         return "CharacterDataModel "
@@ -73,6 +129,8 @@ public class CharacterDataModel {
                 + ", name=" + name
                 + ", health=" + health
                 + ", damage=" + damage
+                + ", movementData=" + movementData
+                + ", attackData=" + attackData
                 + "]";
     }
 
@@ -82,12 +140,14 @@ public class CharacterDataModel {
                 && ((CharacterDataModel) o).character.equals(this.character)
                 && ((CharacterDataModel) o).name.equals(this.name)
                 && ((CharacterDataModel) o).health == this.health
-                && ((CharacterDataModel) o).damage == this.damage;
+                && ((CharacterDataModel) o).damage == this.damage
+                && ((CharacterDataModel) o).attackData == this.attackData
+                && ((CharacterDataModel) o).movementData == this.movementData;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(character, name, health, damage);
+        return Objects.hash(character, name, health, damage, attackData, movementData);
     }
 
 }
