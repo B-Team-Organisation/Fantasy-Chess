050bb2995460558ebf2baa811317fa92fe478ea2
Jacinto Schwarzwälder <schwarzwaelder.jacinto@gmail.com>
2024-11-04 13:23:58 +0100

    [Refactor] Changing to PatternModel and adding description

--- common/src/main/java/models/CharacterDataModel.java
+++ common/src/main/java/models/CharacterDataModel.java
@@ -1,153 +1,117 @@
 package models;
 
-import entities.CharacterEntity;
-
+import java.util.Arrays;
 import java.util.Objects;
 
 /**
  * Data model for character entities
  * <p>
- * This class represents a Data Transferable Object for static character data.
- * It contains static attributes about a character.
- * <p>
- * A {@link CharacterDataModel} can only have one of {@link AttackDataModel} and {@link MovementDataModel}.
- * This is done to adhere to the game logic.
+ * This class represents a Character Model to model characters.
+ * It contains static attributes about a character, including all
+ * movement and attack patterns as arrays of {@link PatternDataModel}
+ * and {@link AttackPatternDataModel}.
  *
  * @author Jacinto
  * @version 1.0
  */
 public class CharacterDataModel {
-    private CharacterEntity character;
     private String name;
+    private String description;
     private int health;
-    private int damage;
-    private AttackDataModel attackData;
-    private MovementDataModel movementData;
+    private AttackPatternDataModel[] attackPatterns;
+    private PatternDataModel[] movementPatterns;
 
 
     /**
-     * Creates a new CharacterDataModel with attack data.
+     * Creates a new CharacterDataModel.
      *
-     * @param character - reference to the {@link CharacterEntity}
      * @param name - name of the character
-     * @param health - the health of the character
-     * @param damage - the damage from the character
-     * @param attackData - the attack pattern that the character is set to
+     * @param description - a description for the character
+     * @param health - the damage from the character
+     * @param attackPatterns - all attack patterns that the character can perform
+     * @param movementPatterns - the movement pattern that the character can perform
      */
-    public CharacterDataModel(CharacterEntity character, String name, int health, int damage, AttackDataModel attackData) {
-        this.character = character;
+    public CharacterDataModel(
+            String name,
+            String description,
+            int health,
+            AttackPatternDataModel[] attackPatterns,
+            PatternDataModel[] movementPatterns
+    ) {
         this.name = name;
+        this.description = description;
         this.health = health;
-        this.damage = damage;
-        this.attackData = attackData;
-    }
-
-    /**
-     * Creates a new CharacterDataModel with movement data.
-     *
-     * @param character - reference to the {@link CharacterEntity}
-     * @param name - name of the character
-     * @param health - the health of the character
-     * @param damage - the damage from the character
-     * @param movementData - the movement pattern that the character is set to
-     */
-    public CharacterDataModel(CharacterEntity character, String name, int health, int damage, MovementDataModel movementData) {
-        this.character = character;
-        this.name = name;
-        this.health = health;
-        this.damage = damage;
-        this.movementData = movementData;
-    }
-
-    /**
-     * Creates a new CharacterDataModel without movement or attack data.
-     *
-     * @param character - reference to the {@link CharacterEntity}
-     * @param name - name of the character
-     * @param health - the health of the character
-     * @param damage - the damage from the character
-     */
-    public CharacterDataModel(CharacterEntity character, String name, int health, int damage) {
-        this.character = character;
-        this.name = name;
-        this.health = health;
-        this.damage = damage;
-    }
-
-    public CharacterEntity getCharacter() {
-        return this.character;
+        this.attackPatterns = Arrays.copyOf(attackPatterns, attackPatterns.length);
+        this.movementPatterns = Arrays.copyOf(movementPatterns, attackPatterns.length);
     }
 
     public String getName() {
         return this.name;
     }
 
+    public String getDescription() {
+        return this.description;
+    }
+
     public int getHealth() {
         return this.health;
     }
 
-    public int getDamage() {
-        return this.damage;
+    public AttackPatternDataModel[] getAttackPatterns() {
+        return Arrays.copyOf(attackPatterns, attackPatterns.length);
     }
 
-    public AttackDataModel getAttackData() {
-        return this.attackData;
-    }
-
-    public MovementDataModel getMovementData() {
-        return this.movementData;
-    }
-
-    public void setCharacter(CharacterEntity character) {
-        this.character = character;
+    public PatternDataModel[] getMovementPatterns() {
+        return Arrays.copyOf(movementPatterns, attackPatterns.length);
     }
 
     public void setName(String name) {
         this.name = name;
     }
 
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
     public void setHealth(int health) {
         this.health = health;
     }
 
-    public void setDamage(int damage) {
-        this.damage = damage;
+    public void setAttackPatterns(AttackPatternDataModel[] attackPatterns) {
+        this.attackPatterns = attackPatterns;
     }
 
-    public void setAttackData(AttackDataModel attackData) {
-        this.attackData = attackData;
-    }
-
-    public void setMovementData(MovementDataModel movementData) {
-        this.movementData = movementData;
+    public void setMovementPatterns(PatternDataModel[] movementPatterns) {
+        this.movementPatterns = movementPatterns;
     }
 
     @Override
     public String toString() {
         return "CharacterDataModel "
-                + "[character=" + character.toString()
-                + ", name=" + name
+                + "[name=" + name
+                + ", description=" + description
                 + ", health=" + health
-                + ", damage=" + damage
-                + ", movementData=" + movementData
-                + ", attackData=" + attackData
+                + ", movementData=" + Arrays.toString(movementPatterns)
+                + ", attackData=" + Arrays.toString(attackPatterns)
                 + "]";
     }
 
     @Override
     public boolean equals(Object o) {
         return o instanceof CharacterDataModel
-                && ((CharacterDataModel) o).character.equals(this.character)
                 && ((CharacterDataModel) o).name.equals(this.name)
+                && ((CharacterDataModel) o).description.equals(this.description)
                 && ((CharacterDataModel) o).health == this.health
-                && ((CharacterDataModel) o).damage == this.damage
-                && ((CharacterDataModel) o).attackData == this.attackData
-                && ((CharacterDataModel) o).movementData == this.movementData;
+                && Arrays.equals(((CharacterDataModel) o).movementPatterns, this.movementPatterns)
+                && Arrays.equals(((CharacterDataModel) o).attackPatterns, attackPatterns);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(character, name, health, damage, attackData, movementData);
+        return Objects.hash(
+                name, description, health,
+                Arrays.hashCode(movementPatterns),
+                Arrays.hashCode(attackPatterns));
     }
 
 }
