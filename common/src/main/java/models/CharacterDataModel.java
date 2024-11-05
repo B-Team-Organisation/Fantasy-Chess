33c59a3846f6bf14e93bc09c6496901a9a260867
Jacinto Schwarzwälder <122536229+Jacinto-S@users.noreply.github.com>
2024-11-05 09:52:33 +0100

    [10] Character Layout (#31)
    * [Draft] character layout draft
    
    * [Feature] Adding CharacterDataModel
    
    * [Feature] Adding AttackData and MovementData.
    
    * [Refactor] Changing to PatternModel and adding description

---
+++ common/src/main/java/models/CharacterDataModel.java
@@ -0,0 +1,117 @@
+package models;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+/**
+ * Data model for character entities
+ * <p>
+ * This class represents a Character Model to model characters.
+ * It contains static attributes about a character, including all
+ * movement and attack patterns as arrays of {@link PatternDataModel}
+ * and {@link AttackPatternDataModel}.
+ *
+ * @author Jacinto
+ * @version 1.0
+ */
+public class CharacterDataModel {
+    private String name;
+    private String description;
+    private int health;
+    private AttackPatternDataModel[] attackPatterns;
+    private PatternDataModel[] movementPatterns;
+
+
+    /**
+     * Creates a new CharacterDataModel.
+     *
+     * @param name - name of the character
+     * @param description - a description for the character
+     * @param health - the damage from the character
+     * @param attackPatterns - all attack patterns that the character can perform
+     * @param movementPatterns - the movement pattern that the character can perform
+     */
+    public CharacterDataModel(
+            String name,
+            String description,
+            int health,
+            AttackPatternDataModel[] attackPatterns,
+            PatternDataModel[] movementPatterns
+    ) {
+        this.name = name;
+        this.description = description;
+        this.health = health;
+        this.attackPatterns = Arrays.copyOf(attackPatterns, attackPatterns.length);
+        this.movementPatterns = Arrays.copyOf(movementPatterns, attackPatterns.length);
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public int getHealth() {
+        return this.health;
+    }
+
+    public AttackPatternDataModel[] getAttackPatterns() {
+        return Arrays.copyOf(attackPatterns, attackPatterns.length);
+    }
+
+    public PatternDataModel[] getMovementPatterns() {
+        return Arrays.copyOf(movementPatterns, attackPatterns.length);
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public void setAttackPatterns(AttackPatternDataModel[] attackPatterns) {
+        this.attackPatterns = attackPatterns;
+    }
+
+    public void setMovementPatterns(PatternDataModel[] movementPatterns) {
+        this.movementPatterns = movementPatterns;
+    }
+
+    @Override
+    public String toString() {
+        return "CharacterDataModel "
+                + "[name=" + name
+                + ", description=" + description
+                + ", health=" + health
+                + ", movementData=" + Arrays.toString(movementPatterns)
+                + ", attackData=" + Arrays.toString(attackPatterns)
+                + "]";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return o instanceof CharacterDataModel
+                && ((CharacterDataModel) o).name.equals(this.name)
+                && ((CharacterDataModel) o).description.equals(this.description)
+                && ((CharacterDataModel) o).health == this.health
+                && Arrays.equals(((CharacterDataModel) o).movementPatterns, this.movementPatterns)
+                && Arrays.equals(((CharacterDataModel) o).attackPatterns, attackPatterns);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(
+                name, description, health,
+                Arrays.hashCode(movementPatterns),
+                Arrays.hashCode(attackPatterns));
+    }
+
+}
