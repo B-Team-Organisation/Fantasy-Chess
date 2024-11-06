fe33dc0e644eb39b5459e4abd07a47def3b9bc0c
Jacinto Schwarzwälder <122536229+Jacinto-S@users.noreply.github.com>
2024-11-06 13:40:16 +0100

    [#21] implementing CharacterEntity (#41)

--- common/src/main/java/entities/CharacterEntity.java
+++ common/src/main/java/entities/CharacterEntity.java
@@ -1,5 +1,80 @@
 package entities;
 
+import models.Vector2D;
+import models.CharacterDataModel;
+
+import java.util.Objects;
+
+/**
+ * Entity representing the state of a character.
+ * <p>
+ * Contains a reference to the {@link CharacterDataModel} and
+ * live attributes of the character like current health and
+ * position.
+ *
+ * @author Jacinto, Adnan
+ * @version 1.0
+ */
 public class CharacterEntity {
-    //placeholder, tbd
+    private CharacterDataModel characterBaseModel;
+    private int health;
+    private Vector2D position;
+
+    /**
+     * Creates a new Character
+     *
+     * @param characterBaseModel Reference to the character in question
+     * @param health Current health of the character
+     * @param position Current position of the character
+     */
+    public CharacterEntity(CharacterDataModel characterBaseModel, int health, Vector2D position) {
+        this.characterBaseModel = characterBaseModel;
+        this.health = health;
+        this.position = position;
+    }
+
+    public CharacterDataModel getCharacterBaseModel() {
+        return this.characterBaseModel;
+    }
+
+    public int getHealth() {
+        return this.health;
+    }
+
+    public Vector2D getPosition() {
+        return this.position;
+    }
+
+    public void setCharacterBaseModel(CharacterDataModel characterBaseModel) {
+        this.characterBaseModel = characterBaseModel;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public void setPosition(Vector2D position) {
+        this.position = position;
+    }
+
+    @Override
+    public String toString() {
+        return "CharacterEntity [characterBaseModel=" + characterBaseModel.toString()
+                + ", health=" + health
+                + ", position =" + position +  "]";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return o instanceof CharacterEntity
+                && ((CharacterEntity) o).characterBaseModel.equals(this.characterBaseModel)
+                && ((CharacterEntity) o).health == this.health
+                && ((CharacterEntity) o).position.equals(this.position);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(characterBaseModel, health, position);
+    }
+
 }
