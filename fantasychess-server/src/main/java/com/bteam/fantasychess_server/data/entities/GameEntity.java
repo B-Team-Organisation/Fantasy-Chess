04f491f7247f134ab3ae704052f17d2c40724f66
Marc Matija <marcmatija.mm@gmail.com>
2024-11-08 15:05:12 +0100

    Further Refinements and Entity Implementations

--- fantasychess-server/src/main/java/com/bteam/fantasychess_server/data/entities/GameEntity.java
+++ fantasychess-server/src/main/java/com/bteam/fantasychess_server/data/entities/GameEntity.java
@@ -1,5 +1,6 @@
 package com.bteam.fantasychess_server.data.entities;
 
+import enums.GameStatus;
 import jakarta.persistence.*;
 import org.hibernate.annotations.JdbcTypeCode;
 import org.hibernate.type.SqlTypes;
@@ -14,6 +15,16 @@ public class GameEntity {
     @Column(name = "id", nullable = false)
     @JdbcTypeCode(SqlTypes.UUID)
     private UUID id;
+
+    @Column(name = "turn")
+    private int Turn;
+
+    @Column
+    private int maxTurnSeconds;
+
+    @Column
+    private GameStatus status;
+
 
     @OneToMany
     private List<CharacterDataEntity> characters;
