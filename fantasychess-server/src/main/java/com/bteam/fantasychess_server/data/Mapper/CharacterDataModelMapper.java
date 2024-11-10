2bf8c4d314cc0cd99afd1358a10f3de029daa617
casqan <marcmatija.mm@gmail.com>
2024-11-10 23:09:54 +0100

    [#25] borken methods now throw a NotImplementedException

--- fantasychess-server/src/main/java/com/bteam/fantasychess_server/data/Mapper/CharacterDataModelMapper.java
+++ fantasychess-server/src/main/java/com/bteam/fantasychess_server/data/Mapper/CharacterDataModelMapper.java
@@ -1,15 +1,11 @@
 package com.bteam.fantasychess_server.data.Mapper;
 
+import Exceptions.NotImplementedException;
 import com.bteam.fantasychess_server.data.entities.CharacterDataEntity;
 import models.CharacterDataModel;
 
 public class CharacterDataModelMapper {
     public CharacterDataModel EntityToModel(CharacterDataEntity characterEntity) {
-        return new CharacterDataModel(
-                characterEntity.getId(),
-                characterEntity.getName(),
-                characterEntity.getHealth(),
-                characterEntity.getAttackPatterns(),
-                characterEntity.getMovementPatterns());
+        throw new NotImplementedException("EntityToModel");
     }
 }
--- fantasychess-server/src/main/java/com/bteam/fantasychess_server/data/Mapper/GameMapper.java
+++ fantasychess-server/src/main/java/com/bteam/fantasychess_server/data/Mapper/GameMapper.java
@@ -1,5 +1,6 @@
 package com.bteam.fantasychess_server.data.Mapper;
 
+import Exceptions.NotImplementedException;
 import com.bteam.fantasychess_server.data.entities.GameEntity;
 import com.bteam.fantasychess_server.data.repositories.GameRepository;
 import models.GameModel;
@@ -12,6 +13,6 @@ public class GameMapper {
     GameRepository gameRepository;
 
     public static GameModel EntityToModel(GameEntity gameEntity) {
-
+        throw new NotImplementedException("EntityToModel");
     }
 }
