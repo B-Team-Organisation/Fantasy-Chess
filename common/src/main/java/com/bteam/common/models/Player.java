659143ffd1a731b6599f2a71de556f9e0ee7f49b
Albano <albigoal@outlook.com>
2024-11-16 13:54:41 +0100

    [Feature] Character dependency from Player

--- common/src/main/java/com/bteam/common/models/Player.java
+++ common/src/main/java/com/bteam/common/models/Player.java
@@ -1,19 +1,24 @@
 package com.bteam.common.models;
 
+import java.util.ArrayList;
+import com.bteam.common.entities.CharacterEntity;
+import java.util.List;
 import java.util.Objects;
+import java.util.UUID;
 
 /**
  * A class representing a player.
  * <p>
- * Represents a player in the game, holding their associated name, playerId and status.
+ * Represents a player in the game, holding their associated name, playerId, list of characters and status.
  *
- * @author Adnan
- * @version 1.0
+ * @author Adnan, Albano
+ * @version 1.1
  */
 public class Player {
     private String username;
-    private String playerId;
+    private UUID playerId;
     private Status status;
+    private List<CharacterEntity> characters;
 
     enum Status{
         NOT_READY,
@@ -26,11 +31,13 @@ public class Player {
      *
      * @param username name of the players
      * @param playerId id of the player
+     * @param characters list of characters associated with the player
      */
-    public Player(String username, String playerId) {
+    public Player(String username, UUID playerId, List<CharacterEntity> characters) {
         this.username = username;
         this.playerId = playerId;
         this.status = Status.NOT_READY;
+        this.characters = new ArrayList<>();
     }
 
 
@@ -38,12 +45,16 @@ public class Player {
         return username;
     }
 
-    public String getPlayerId() {
+    public UUID getPlayerId() {
         return playerId;
     }
 
     public Status getStatus() {
         return status;
+    }
+
+    public List<CharacterEntity> getCharacters(){
+        return characters;
     }
 
     public void setUsername(String username) {
@@ -54,18 +65,52 @@ public class Player {
         this.status = status;
     }
 
+    public void setCharacters(List<CharacterEntity> characters) {
+        this.characters = characters != null ? characters : new ArrayList<>();
+    }
+
+
+    /**
+     * Adds a character to the player's character list.
+     *
+     * @param character The character to add
+     */
+    public boolean addCharacter(CharacterEntity character) {
+        if (character != null && !characters.contains(character)) {
+            return characters.add(character);
+        }
+        return false;
+    }
+
+    /**
+     * Removes a character from the player's character list.
+     *
+     * @param character The character to remove
+     */
+    public boolean removeCharacter(CharacterEntity character) {
+        if (character != null && characters.contains(character)) {
+            return characters.remove(character);
+        }
+        return false;
+    }
+
 
     @Override
     public String toString() {
-        return "Username: " + username + "PlayerId: " + playerId + "Status: "+ status;
+        return "Username: " + username + "PlayerId: " + playerId + "Status: "+ status+ "Characters: " + characters;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(playerId, username, status);
+        return Objects.hash(playerId, username, status, characters);
     }
 
     public boolean equals(Object o){
-        return o instanceof Player && ((Player)o).playerId.equals(playerId);
+        return o instanceof Player
+                && ((Player)o).playerId.equals(playerId)
+                && ((Player)o).username.equals(username)
+                && ((Player)o).status.equals(status)
+                && ((Player)o).characters.equals(characters);
+
     }
 }
