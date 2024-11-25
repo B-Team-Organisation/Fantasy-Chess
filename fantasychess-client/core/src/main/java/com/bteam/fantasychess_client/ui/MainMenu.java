a9772323e5240f7274c7d25682604e89ff459f1b
Lukas Walker <lukas.walker@student.hs-rm.de>
2024-11-25 16:29:05 +0100

    Fixed main menu

--- fantasychess-client/core/src/main/java/com/bteam/fantasychess_client/ui/MainMenu.java
+++ fantasychess-client/core/src/main/java/com/bteam/fantasychess_client/ui/MainMenu.java
@@ -277,18 +277,18 @@ public class MainMenu extends ScreenAdapter {
     // testdata,to check with online data
     private List<LobbyModel> showLobbies() {
         List<LobbyModel> lobbies = new ArrayList<>();
-        lobbies.add(new LobbyModel("Lukas",new ArrayList<>(),null,"Lobyb1"));
-        lobbies.add(new LobbyModel("Hana",new ArrayList<>(),null,"Lobyb1"));
-        lobbies.add(new LobbyModel("Luxort",new ArrayList<>(),null,"Lobyb1"));
-        lobbies.add(new LobbyModel("Zhuxin",new ArrayList<>(),null,"Lobyb1"));
-        lobbies.add(new LobbyModel("Beras",new ArrayList<>(),null,"Lobyb1"));
-        lobbies.add(new LobbyModel("Ino",new ArrayList<>(),null,"Lobyb1"));
-        lobbies.add(new LobbyModel("Kalavoi",new ArrayList<>(),null,"Lobyb1"));
-        lobbies.add(new LobbyModel("Albert",new ArrayList<>(),null,"Lobyb1"));
-        lobbies.add(new LobbyModel("Sius",new ArrayList<>(),null,"Lobyb1"));
-        lobbies.add(new LobbyModel("Demonzone",new ArrayList<>(),null,"Lobyb1"));
-        lobbies.add(new LobbyModel("Angels",new ArrayList<>(),null,"Lobyb1"));
-        lobbies.add(new LobbyModel("Classrom of",new ArrayList<>(),null,"Lobyb1"));
+        lobbies.add(new LobbyModel("Lukas",new ArrayList<>(),null,"Lobyb1",2));
+        lobbies.add(new LobbyModel("Hana",new ArrayList<>(),null,"Lobyb1",2));
+        lobbies.add(new LobbyModel("Luxort",new ArrayList<>(),null,"Lobyb1",2));
+        lobbies.add(new LobbyModel("Zhuxin",new ArrayList<>(),null,"Lobyb1",2));
+        lobbies.add(new LobbyModel("Beras",new ArrayList<>(),null,"Lobyb1",2));
+        lobbies.add(new LobbyModel("Ino",new ArrayList<>(),null,"Lobyb1",2));
+        lobbies.add(new LobbyModel("Kalavoi",new ArrayList<>(),null,"Lobyb1",2));
+        lobbies.add(new LobbyModel("Albert",new ArrayList<>(),null,"Lobyb1",2));
+        lobbies.add(new LobbyModel("Sius",new ArrayList<>(),null,"Lobyb1",2));
+        lobbies.add(new LobbyModel("Demonzone",new ArrayList<>(),null,"Lobyb1",2));
+        lobbies.add(new LobbyModel("Angels",new ArrayList<>(),null,"Lobyb1",2));
+        lobbies.add(new LobbyModel("Classrom of",new ArrayList<>(),null,"Lobyb1",2));
         return lobbies;
     }
 
