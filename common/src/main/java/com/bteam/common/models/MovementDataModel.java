0b98b36784cebc1232e4bab1fba7d58a1ebb2a90
adbennis <adnan.bennis@gmx.de>
2024-11-28 09:42:18 +0100

    [Refactor] Refactor Attack and Movementpattern (#94)
    Co-authored-by: abenn001 <adnan.bennis@student.hs-rm.de>

--- common/src/main/java/com/bteam/common/models/MovementDataModel.java
+++ common/src/main/java/com/bteam/common/models/MovementDataModel.java
@@ -6,37 +6,37 @@ import com.bteam.common.entities.CharacterEntity;
 /**
  * Represents the targeted movement of a character on the playing field.
  * <p>
- * A movement model contains of a {@link CharacterEntity} and its position as
+ * A movement model contains of a {@link String} and its position as
  * a {@link Vector2D}
  *
  * @author Jacinto
  * @version 1.0
  */
 public class MovementDataModel {
-    CharacterEntity characterEntity;
+    private String characterId;
     Vector2D movementVector;
 
     /**
      * Create a new movement model from the current position and the movement vector.
      *
-     * @param characterEntity reference to the character that is supposed to be moved
+     * @param characterId reference to the character that is supposed to be moved
      * @param movementVector a 2DVector pointing to the absolut position the character should move to
      */
-    public MovementDataModel(CharacterEntity characterEntity, Vector2D movementVector) {
-        this.characterEntity = characterEntity;
+    public MovementDataModel(String characterId, Vector2D movementVector) {
+        this.characterId = characterId;
         this.movementVector = movementVector;
     }
 
-    public CharacterEntity getCharacterEntity() {
-        return characterEntity;
+    public String getCharacterId() {
+        return characterId;
     }
 
     public Vector2D getMovementVector() {
         return movementVector;
     }
 
-    public void setCharacterEntity(CharacterEntity characterEntity) {
-        this.characterEntity = characterEntity;
+    public void setCharacterId(String characterId) {
+        this.characterId = characterId;
     }
 
     public void setMovementVector(Vector2D movementVector) {
@@ -45,19 +45,19 @@ public class MovementDataModel {
 
     @Override
     public String toString() {
-        return "MovementVector: " + movementVector + "\nCharacterEntity:\n" + characterEntity;
+        return "MovementVector: " + movementVector + "\nCharacterEntity:\n" + characterId;
     }
 
     @Override
     public boolean equals(Object o) {
         return o instanceof MovementDataModel
-                && this.characterEntity.equals(((MovementDataModel) o).characterEntity)
+                && this.characterId.equals(((MovementDataModel) o).characterId)
                 && movementVector.equals(((MovementDataModel) o).movementVector);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(characterEntity, movementVector);
+        return Objects.hash(characterId, movementVector);
     }
 
 }
