f4d684d3a3c6de300cc7e8116907e8a6deaa7485
casqan <marcmatija.mm@gmail.com>
2025-02-01 15:45:45 +0100

    [#136] updated dto

--- common/src/main/java/com/bteam/common/dto/PlayerDTO.java
+++ common/src/main/java/com/bteam/common/dto/PlayerDTO.java
@@ -3,18 +3,21 @@ package com.bteam.common.dto;
 import com.bteam.common.models.Player;
 import com.bteam.common.utils.JsonWriter;
 
-public class PlayerDTO implements JsonDTO{
+public class PlayerDTO implements JsonDTO {
     private final String username;
     private final String playerId;
+    private final String status;
 
     public PlayerDTO(Player player) {
         this.username = player.getUsername();
         this.playerId = player.getPlayerId();
+        this.status = player.getStatus().toString();
     }
 
-    public PlayerDTO(String username, String playerId) {
+    public PlayerDTO(String username, String playerId, String status) {
         this.username = username;
         this.playerId = playerId;
+        this.status = status;
     }
 
     public String getUsername() {
@@ -25,9 +28,14 @@ public class PlayerDTO implements JsonDTO{
         return playerId;
     }
 
+    public String getStatus() {
+        return status;
+    }
+
     @Override
     public String toJson() {
         return new JsonWriter().writeKeyValue("username", getUsername())
-            .and().writeKeyValue("playerId", getPlayerId()).toString();
+                .and().writeKeyValue("playerId", getPlayerId())
+                .and().writeKeyValue("status", getStatus()).toString();
     }
 }
